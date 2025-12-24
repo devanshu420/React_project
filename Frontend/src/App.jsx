1c4e6ffa640d0cf5dfb10cd0371847e812718d9c
Devanshu Sharma <devanshu11122005@gmail.com>
2025-12-24 18:32:43 +0530

    Create a folder strucutre of frontend and backend.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
 
 const App = () => {
   return (
     <div>
-      App
+      <Home />
+      <Login />
+      <Register />
     </div>
   )
 }
